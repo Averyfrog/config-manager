b78929ca16785697c9d022f6070a159da2eb1731
AveryFrog <pixels128yt@gmail.com>
2026-01-26 12:05:06 +0000

    fix package name

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.rustPlatform.buildRustPackage {
   pname = "constellate";
-  version = "0.1.1";
+  version = "0.2.0";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
 }
